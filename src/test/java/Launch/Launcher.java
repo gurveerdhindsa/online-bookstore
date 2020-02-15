4bf26463ead47a94a81c5fc17db6a00b7f690e8b
rohank07 <rohank_17@hotmail.ca>
2020-02-15 12:43:54 -0800

    Created User, Owner class & tests

--- src/test/java/Launch/Launcher.java
+++ src/test/java/Launch/Launcher.java
@@ -3,10 +3,16 @@ package Launch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 @SpringBootApplication
 public class Launcher {
 
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+            .createEntityManagerFactory("BookStore-db");
     public static void main(String [] args) {
+
         SpringApplication.run(Launcher.class);
 
     }
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,90 @@
+import bookstore.Book;
+import bookstore.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * UserTest class
+ */
+
+public class UserTest {
+    User user;
+
+    @Before
+    public void setUp() {
+        user = new User(1, "Abu", "Abdul");
+
+    }
+    @After
+    public void tearDown() {
+        user = null;
+    }
+
+    /**
+     * Method: getFirstName()
+     */
+    @Test
+    public void testGetFirstName() throws Exception {
+        assertEquals("Abu", user.getFirstName());
+    }
+    /**
+     * Method: getLastName()
+     */
+    @Test
+    public void testGetLastName() throws Exception {
+        assertEquals("Abdul", user.getLastName());
+    }
+    /**
+     * Method: getId()
+     */
+    @Test
+    public void testId() throws Exception {
+        assertEquals(1, user.getUserId());
+    }
+    /**
+     * Method: setId()
+     */
+
+    @Test
+    public void testSetId()throws Exception
+    {
+        user.setUserId(2);
+        assertEquals(2, user.getUserId());
+    }
+
+    /**
+     * Method: setFirstName()
+     */
+    @Test
+    public void testSetFirstName()throws Exception
+    {
+        user.setFirstName("Gururu");
+        assertEquals("Gururu", user.getFirstName());
+    }
+    /**
+     * Method: setLastName()
+     */
+    @Test
+    public void testSetLastName()throws Exception
+    {
+        user.setLastName("Barolia");
+        assertEquals("Barolia", user.getLastName());
+    }
+
+    /**
+     * Method: isEqual(Object obj)
+     */
+    @Test
+    public void testIsEqual() throws Exception {
+        User user2 = new User( 1 ,"Abu", "Abdul");
+        assertTrue(user.isEqual(user2));
+    }
+
+
+
+
+
+}
+
