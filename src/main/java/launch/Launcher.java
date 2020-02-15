46c61e7d33baee9e12717642e59565e62f226735
rohank07 <rohank_17@hotmail.ca>
2020-02-15 13:03:48 -0800

    updated pom.xml

--- src/main/java/launch/Launcher.java
+++ src/main/java/launch/Launcher.java
@@ -1,4 +1,4 @@
-package Launch;
+package launch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +9,7 @@ import javax.persistence.Persistence;
 @SpringBootApplication
 public class Launcher {
 
-    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
-            .createEntityManagerFactory("BookStore-db");
+
     public static void main(String [] args) {
 
         SpringApplication.run(Launcher.class);
