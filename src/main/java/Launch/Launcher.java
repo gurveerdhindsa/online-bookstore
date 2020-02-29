2c4e044ef979872b7ad0cd40a23650951625ec82
Gurveer Dhindsa <gurveerdhindsa@hotmail.com>
2020-02-29 15:14:39 -0500

    Display all books on frontend Fixed /books endpoint to return correct JSON format

--- src/main/java/Launch/Launcher.java
+++ src/main/java/Launch/Launcher.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class Launcher {
-
-
     public static void main(String [] args) {
-
         SpringApplication.run(Launcher.class);
-
     }
 }*/
--- src/main/java/Repository/Launcher.java
+++ src/main/java/Repository/Launcher.java
@@ -1,11 +1,7 @@
 package Repository;
-import Bookstore.Book;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 /**
  * Start of the application
@@ -16,26 +12,20 @@ public class Launcher {
     BookRepository bookRepo;
 
     public static void main(String[] args) {
-
         SpringApplication.run(Launcher.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
-
-            Book book1 = new Book(1, "9781449474256", "milk and honey", "Rupi Kaur", "Andrews McMeel Publishing", 35);
-            bookRepo.save((book1));
-
-            Book book2 = new Book(2, "9780374201234", "The Mamba Mentality: How I play", "Kobe Bryant", "Farrar, Straus And Giroux", 45);
-            bookRepo.save(book2);
-
-            Book book3 = new Book(3, "978037424", "The Mamba", "Kobe", " Giroux", 45);
-            bookRepo.save(book3);
-
-
-
-        };
-
-    }
+//    @Bean
+//    CommandLineRunner runner() {
+//        return args -> {
+//            Book book1 = new Book(1, "9781449474256", "milk and honey", "Rupi Kaur", "Andrews McMeel Publishing", 35);
+//            bookRepo.save((book1));
+//
+//            Book book2 = new Book(2, "9780374201234", "The Mamba Mentality: How I play", "Kobe Bryant", "Farrar, Straus And Giroux", 45);
+//            bookRepo.save(book2);
+//
+//            Book book3 = new Book(3, "978037424", "The Mamba", "Kobe", " Giroux", 45);
+//            bookRepo.save(book3);
+//        };
+//    }
 }
