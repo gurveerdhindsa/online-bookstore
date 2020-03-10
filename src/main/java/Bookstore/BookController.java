dda42fcc274a0a0a50bf62ab9b9f358680ff5a84
rohank07 <rohank_17@hotmail.ca>
2020-03-09 20:55:42 -0700

    Placed controller class back into Bookstore package

--- src/main/java/Bookstore/BookController.java
+++ src/main/java/Bookstore/BookController.java
@@ -1,4 +1,4 @@
-package Repository;
+package Bookstore;
 
 import Bookstore.Book;
 import Repository.BookRepository;
--- src/test/java/Bookstore/BookControllerTest.java
+++ src/test/java/Bookstore/BookControllerTest.java
@@ -1,7 +1,6 @@
 package Bookstore;
 
 
-import Repository.BookController;
 import Repository.BookRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
@@ -12,10 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
